f1c7caacb1655d46a605a58c05410ce192ee0d68
minseo joen <minseo@gcn.dmlab.com>
2024-02-15 20:33:27 +0900

    feat:fix main.py

--- Recbole/main.py
+++ Recbole/main.py
@@ -1,15 +1,30 @@
 from recbole.config import Config
 from recbole.quick_start import run_recbole
 import torch
-print(torch.cuda.is_available())
-config_file_SASREC=[
-    './configs/Training/train.yaml',
-    './configs/Model/Multi_VAE.yaml',
-    './configs/EVAL/eval.yaml',
-    './configs/Dataset/ML_sequence.yaml',
-    './configs/ENV/env.yaml',
-    ]
-config_file_list2=['./configs/EVAL/eval2.yaml','./configs/ENV/env.yaml']
-config = Config(config_file_list=config_file_SASREC)
+import argparse
 
-run_recbole(dataset=config['dataset'], model=config['model'], config_file_list = config_file_list2)
+def get_config_combination_list(model_name):
+    '''
+    txt 파일의 combination 조합을 읽어오는 함수
+    '''
+    comb_list = None
+    model_name_lower = model_name.lower()
+    with open("./configs/config_combination.txt", "r") as file:
+        for line in file:
+            splited_line = line.split(' ')
+            if splited_line[0] == model_name_lower:
+                comb_list = splited_line[1:]
+    if comb_list == None:
+        raise Exception("모델 config comb가 존재하지 않습니다.")
+    return comb_list
+
+if __name__ == "__main__":
+    #arg parser
+    parser = argparse.ArgumentParser(description='Input Model Name.')
+    parser.add_argument('model', type=str)
+    parser.add_argument('--dataset',default='ML',type=str)
+    args = parser.parse_args()    
+    
+    # run
+    run_recbole(dataset=args.dataset, model=args.model, config_file_list = get_config_combination_list(args.model))
+    
